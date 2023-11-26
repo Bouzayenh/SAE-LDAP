dba71f4e853f47a67cc9f4541ba1531fe7332626
raphael malka <raphael.malka@student.vinci.be>
2023-11-27 00:04:40 +0100

    refactor listOfUsers page css

--- php/src/view/userList.php
+++ php/src/view/userList.php
@@ -7,25 +7,26 @@
     <link rel="stylesheet" type="text/css" href="../src/assets/styles.css">
 </head>
 <body>
-    <h1>Liste des Utilisateurs</h1>
-    <div class="users-list">
-        <?php
-            require_once('../src/controller/ControllerLDAP.php');
+    <div class="users-container">
+        <h1>Liste des Utilisateurs</h1>
+        <div class="users-list">
+            <?php
+                require_once('../src/controller/ControllerLDAP.php');
 
-            $users = App\LDAP\controller\ControllerLDAP::listUsers();
-            if (!empty($users)) {
-                echo "<ul>";
-                foreach ($users as $user) {
-                    $cn = isset($user['cn']) ? htmlspecialchars($user['cn']) : "Nom Inconnu";
-                    $mail = isset($user['mail']) ? htmlspecialchars($user['mail']) : "Email Inconnu";
-                    echo "<li>" . $cn . " - " . $mail . "</li>";
+                $users = App\LDAP\controller\ControllerLDAP::listUsers();
+                if (!empty($users)) {
+                    echo "<ul>";
+                    foreach ($users as $user) {
+                        $cn = isset($user['cn']) ? htmlspecialchars($user['cn']) : "Nom Inconnu";
+                        $mail = isset($user['mail']) ? htmlspecialchars($user['mail']) : "Email Inconnu";
+                        echo "<li><strong>Nom :</strong> " . $cn . "<br><strong>Mail :</strong> " . $mail . "</li>";
+                    }
+                    echo "</ul>";
+                } else {
+                    echo "<p>Aucun utilisateur trouvé.</p>";
                 }
-                echo "</ul>";
-            } else {
-                echo "<p>Aucun utilisateur trouvé.</p>";
-            }
-        ?>
+            ?>
+        </div>
     </div>
 </body>
 </html>
-
